9e38ce119430a2d47d6c666d8e3ae9a00186a0e4
FrancoOlivera <francooliverarojas@gmail.com>
2023-11-07 23:13:10 -0300

    mi proyecto

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = proces.env.PORT || 3001
+
+app.use(express.static('public'));
+
+app.listen(port, ()=>{
+    console.log('Servidor corriendo en el puerto ' + port);
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
